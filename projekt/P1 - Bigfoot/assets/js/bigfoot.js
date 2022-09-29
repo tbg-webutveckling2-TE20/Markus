c1bf86c964e46410dbc6677d077b6789274f2a4f
Markus <markus.arnfjell@edu.lycksele.se>
2022-09-29 11:54:38 +0200

    Ändra lite bilder

--- projekt/P1 - Bigfoot/assets/js/bigfoot.js
+++ projekt/P1 - Bigfoot/assets/js/bigfoot.js
@@ -1,25 +1,32 @@
 function loadBigFoot() {
     document.getElementById('BigFoot').src = 'assets/images/bigfoot.png'
-    document.getElementById('xddEmote').src = 'assets/images/xdd emote.jpg'
+    document.getElementById('xddEmote').src = 'assets/images/xddemote.jpg'
 }
 
 function moveBigFoot() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
     alert('Du hittade Bigfoot!')
     var picture = document.getElementById('BigFoot')
-    var x = Math.random() * 300
-    var y = Math.random() * 300
+    var y = Math.random() * height
+    var x = Math.random() * width
+    
+    picture.style.top = y +'px'
+    picture.style.left = x +'px'
 
-    picture.style.top = x +'px'
-    picture.style.left = y +'px'
+    console.log(width, height)
 }
 
 function xddJumpScare() {
-    const width = window.screen.availWidth;
-    const height = window.screen.availHeight;
-    var picture = document.getElementById('xddEmote')
-    var x = Math.random() * height
-    var y = Math.random() * width
+    alert('xdd')
+    const width = window.innerWidth;
+    const height = window.innerHeight;
 
-    picture.style.top = x +'%'
-    picture.style.bottom = y +'%'
+    var picture = document.getElementById('xddEmote')
+    var y = Math.random() * height
+    var x = Math.random() * width
+
+    picture.style.top = y +'px'
+    picture.style.left = x +'px'
 }
